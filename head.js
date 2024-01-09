50fe92fe7c855d4718c7a906b7df86d72f446902
Cedric.gauthier <cidgaut@gmail.com>
2024-01-08 20:49:42 -0500

    lint

--- head.js
+++ head.js
@@ -4,12 +4,12 @@ const head = function(arr) {
 };
 
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
