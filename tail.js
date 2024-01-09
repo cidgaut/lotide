c72ab5ac7423a3d4d37e3a516249edacc237ca4d
Cedric.gauthier <cidgaut@gmail.com>
2024-01-08 21:33:45 -0500

    Added tail of array, then array to sring conversion to assert

---
+++ tail.js
@@ -0,0 +1,17 @@
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const result = tail(words);
+assertEqual(result.join(', '), "Lighthouse, Labs"); // Use join to convert the array to a string
