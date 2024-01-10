8663ba944023105da7de55b9e679fc380afe0183
Cedric.gauthier <cidgaut@gmail.com>
2024-01-09 20:13:33 -0500

    Added comparison to the arrays to see if they match exactly. Then comapred the true/false with expected

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+
+//Instruction
+//Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+// cannot compare two arrays directly (arr1 === arr2 would be too easy lol)
+const eqArrays = function(arr1, arr2) {
+    // Check if the lengths of the arrays are the same
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+  
+    // Iterate through each element and compare
+    for (let i = 0; i < arr1.length; i++) {
+      // If elements are not equal, return false
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  
+    // If all elements are equal, return true
+    return true;
+  }
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
