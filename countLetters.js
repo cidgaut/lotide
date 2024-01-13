5bf93667f39a25ac22990caf291386124b311a17
Cedric.gauthier <cidgaut@gmail.com>
2024-01-13 12:02:09 -0500

    created an object and created key if key was not in object. Added one each time key was found.

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+const countLetters = function(str) {
+  const results = {};
+
+  //for the duration of the string
+  for (const char of str) {
+
+    //filter out spaces. Continue says don't do anything in the braces.
+    if (char === " ") {
+      continue;
+    }
+
+    //if the character does not already exist in object. create key for character and set value to one.
+    if (!results[char]) {
+      results[char] = 1;
+    }
+
+    //remaining path:the current character is not a space and already exists as a key in object, therefore add count to value of key letter
+    results[char]++;
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
