500c298224c0ab660099a3a14848c558d0a1b69c
Cedric.gauthier <cidgaut@gmail.com>
2024-01-15 14:11:22 -0500

    edited test cases

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,7 +2,7 @@ const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
 
 //test cases
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
   
 assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
