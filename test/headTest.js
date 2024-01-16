0a53a1db44afc8eb585c2a54ac469c0e64f39de6
Cedric.gauthier <cidgaut@gmail.com>
2024-01-15 19:20:12 -0500

    added additional it tests from previous assertEqual. also commented out assertEqual as chai has built in assertion libraries that work best for testing with mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const assertEqual = require('../assertEqual')
+//const assertEqual = require('../assertEqual')
 const head = require('../head');
 
 describe("#head", () => {
@@ -12,8 +12,16 @@ describe("#head", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
 
+  it("returns '5' for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+
+  it("returns 'Hello' for '['Hello', 'Lighthouse', 'Labs']'", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
 });
 
-// test cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// test cases commented out assertEqual as chai is an assertion library. 
+//Chai assertion functions are deliberately designed to play nice with testing frameworks like Mocha.
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
