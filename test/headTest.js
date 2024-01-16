7af4461b29da3d8a56ad24a8e1d1da1fab638bd6
Cedric.gauthier <cidgaut@gmail.com>
2024-01-15 19:01:38 -0500

    added chai and test 'it' cases

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,19 @@
-//requires assertEqual
-//does the .. advise that the required file in in a previous folder?
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual')
 const head = require('../head');
 
+describe("#head", () => {
 
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
+
+// test cases
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
