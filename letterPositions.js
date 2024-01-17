06f26a6ca92fc29dc0b20487d090adce0be4fd23
Cedric.gauthier <cidgaut@gmail.com>
2024-01-16 19:09:46 -0500

    removed eqArrays

--- letterPositions.js
+++ letterPositions.js
@@ -1,31 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  const isEqual = eqArrays(actual, expected);
-  
-  if (isEqual) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-  
-const eqArrays = function(arr1, arr2) {
-  // Check if the lengths of the arrays are the same
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  // Iterate through each element and compare
-  for (let i = 0; i < arr1.length; i++) {
-    // If elements are not equal, return false
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  
-  // If all elements are equal, return true
-  return true;
-};
-
 const letterPositions = function(sentence) {
   const results = {};
   
