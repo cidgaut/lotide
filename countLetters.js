e0c1e37da658e994ee1a6be9cd1114b2d6323c70
Cedric.gauthier <cidgaut@gmail.com>
2024-01-16 19:10:36 -0500

    removed assertEqual

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-////////////////////////////////////////////////////////////////////////////////
-
 const countLetters = function(str) {
   const results = {};
 
